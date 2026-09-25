441fcf37dd0b6ce041006289b634e7af9411cc42
mridul-source <Mridul.38977@stu.upes.ac.in>
2026-09-25 12:47:46 +0530

    Create q17.c

---
+++ day09/q17.c
@@ -0,0 +1,30 @@
+//Q17: Write a program to find the roots of a quadratic equation and categorize them.
+
+#include <stdio.h>
+
+int main() {
+    double a, b, c;
+    double discriminant, root1, root2;
+
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    discriminant = (b * b) - (4 * a * c);
+
+    if (discriminant > 0) {
+       
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("Roots are real and different: %.0f, %.0f\n", root1, root2);
+    } 
+    else if (discriminant == 0) {
+       
+        root1 = -b / (2 * a);
+        printf("Roots are real and same: %.0f\n", root1);
+    } 
+    else {
+      
+        printf("Roots are complex\n");
+    }
+
+    return 0;
+}
